e7af857a6c685f11135afd5d58d43b79656dc87d
ericlewis <ericlewis777@gmail.com>
2019-08-22 17:24:03 -0400

    Initial Commit

---
+++ CarLookup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarLookup
+//
+//  Created by Eric Lewis on 8/22/19.
+//  Copyright © 2019 Eric Lewis, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
